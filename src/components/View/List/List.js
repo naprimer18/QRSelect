cff451345a1155ab8a05a6d67b916b1919698b6d
Vladislav Rebko <v.v.rebko@gmail.com>
2019-04-09 13:05:55 +0300

    fix hover

--- src/components/View/List/List.js
+++ src/components/View/List/List.js
@@ -18,24 +18,22 @@ export default class ListDropdown extends Component {
     };
 
     render() {
-    const {options, filterItems , listOpen, wrapperRef, inputRef, selectedId, focusedId, selectItem, _listRef , valueKey , labelKey , newOptions , classNameByList} = this.props;
+    const {options, filterItems , listOpen, wrapperRef, inputRef, selectedId, focusedId, selectItem, _listRef , valueKey , labelKey , newOptions , classNameByList, setHover} = this.props;
         if ( !inputRef || !listOpen ) {
             return null;
         }
 
         return ReactDOM.createPortal(
-            <div className={"listWrapper"}  /*className={classNameByList ? classNameByList : "listWrapper"}*/ 
-                ref={ ref => _listRef(ref) }
+            <div className={"listWrapper"}  /*className={classNameByList ? classNameByList : "listWrapper"}*/
                 style={ this.getListPosition() }>  
                 {<ul className="dd-list" onClick={ e => e.stopPropagation() } >
                     {options.filter(item => { return item[labelKey].toLowerCase().indexOf(filterItems) >= 0 }).map((item, id) => (
                         <div
                             ref={ list => { if (id === 0) _listRef(list) } }
-                            className={ ( focusedId === item[valueKey] ? "dd-list-item-on-focus" :"dd-list-item" ) + ( selectedId.includes(item[valueKey]) ? " selected-item" : "" ) }
+                            className={ ( focusedId === item[valueKey] ? "dd-list-item-on-focus" : "dd-list-item" ) + ( selectedId.includes(item[valueKey]) ? " selected-item" : "" ) }
                             key={ item[valueKey] }
                             onMouseDown={ (e) => { e.preventDefault() }}
                             onClick={ (e) => {
-                                console.log(item[valueKey]);
                                 selectItem(e, item)
                             }}
                         >
